1e712ab473db62f0875eee98a3784192c010b4f0
daweins <daweins@Microsoft.com>
2020-01-23 22:06:22 +0000

    Working code - cleaning + testing

---
+++ testBootstrap.py
@@ -0,0 +1,114 @@
+import adal
+import requests
+import json
+import uuid
+import time
+import os
+
+# TODO - move these to environment variables to support sovereign endpoints
+authorityBase   = "https://login.microsoftonline.com"
+graphURI        = "https://graph.windows.net"
+redirectUri     = "urn:ietf:wg:oauth:2.0:oob"
+
+# TODO - decide if this should be hardcoded, or left as a random GUID
+appName = str(uuid.uuid4())
+
+print("Starting")
+
+#Tenant Information
+currentDomain = os.environ.get("TENANTNAME") # ex: daweinsatat.onmicrosoft.com - note, because we append this to the userName, don't use the Tenant ID
+userName      = os.environ.get("USERNAME")   # ex: bootstrapadmin if the email address was bootstrapadmin@daweinsatat.onmicrosoft.com
+userPassword  = os.environ.get("USERPWD")    # ex: ImN0tPuttingAnExampleForThis! 
+
+
+# Fail immediately if these aren't populated
+if (currentDomain is None or userName is None or userPassword is None):
+    print("Missing environment variables containing TENANTNAME, USERNAME, or USERPWD. Quitting with code 1 (error)")
+    quit(1)
+else:   
+    print ("Environment variables with user paramters found")
+    
+    # TODO - get this dynamically - not sure if it's static across all tenants
+    roleID = "794bb258-3e31-42ff-9ee4-731a72f62851"
+
+   
+    # Create some useful strings for later
+    clientId    = "1b730954-1685-4b74-9bfd-dac224a7b894"      # PowerShell Client Id - TODO - see if there is a better way to do this, but it"s a good cheat to get on the first rung of the ladder for now
+    authority   = authorityBase + "/" + currentDomain
+    app_url     = graphURI + "/" + currentDomain + "/applications?api-version=1.6"
+    sp_url      = graphURI + "/" + currentDomain + "/servicePrincipals?api-version=1.6"
+    role_url    = graphURI + "/" + currentDomain + "/directoryRoles/" + roleID + "/$links/members?api-version=1.6"
+
+
+  
+
+    # Get our access token to the Graph endpoint. Use a known clientId to bootstrap our way in 
+    try:
+        userEmail = userName + '@' + currentDomain
+        authContext = adal.AuthenticationContext(authority)
+        authResult = authContext.acquire_token_with_username_password(graphURI, userEmail, userPassword, clientId) 
+        if "accessToken" not in authResult:
+            print ("Didn't get an auth token with the provided user credentials. Failing with 1 (error)")
+            # TODO - probably add some more logging, like the error result returned
+            quit(1)
+        else:
+            token = authResult["accessToken"]
+            print("Got the token!")
+    except Exception as e:
+        print ("Error authenticating with the provided user credentials. Failing with 1 (error)")
+        print e
+        quit(1)
+
+
+    # Create the application registration and get its ID
+    try:
+        headers = {
+        "Authorization": "Bearer {}".format(token),
+        "Content-Type":"application/json"
+        }
+
+        appCreateContent = {
+            "displayName" : appName
+        }
+
+        appResponse = requests.post(app_url, headers=headers,data=json.dumps(appCreateContent))
+        appResponseJSON = json.loads(appResponse.content)
+        if "appID" not in appResponseJSON:
+            print ("Failed to create the application registration. Failing with 1 (error)")
+            quit(1)
+        else:
+            appID = appResponseJSON["appId"]
+            print ("Created application regitration with App ID:" + appID)
+    except Exception as e:
+            print("Error creating application registration. Failing with 1 (error)")
+            print e
+            quit(1)
+        
+
+    # Create the service principal associated with the app registration we just created
+    try:
+        servicePrincipalCreateContent = {
+            "appID":appID
+        }
+
+        #TODO - setting the password looks to be a bear, but it needs to be done
+        spResponse = requests.post(sp_url, headers=headers, data=json.dumps(servicePrincipalCreateContent))
+        spID = json.loads(spResponse.content)["objectId"]
+        spURL = graphURI + "/" + currentDomain + "/directoryObjects/" + spID + "/Microsoft.DirectoryServices.ServicePrincipal"
+
+        print ("Newly created spid: " + spID)
+    except Exception as e:
+            print("Error creating the service principal. Failing with 1(error)")
+            print e
+            quit(1)
+    # This may or may not be necessary - tune as needed.
+    print ("Sleeping for AAD propogation")
+    time.sleep(15)
+
+    roleAddContent = {
+        "url": spURL
+    }
+
+    roleCreateResponse = requests.post(role_url, headers=headers, data=json.dumps(roleAddContent))
+
+    print ("Success!")
